9398e37e54c7f86bd05b5add72763eca4c71bf44
Priyanshu farkare <121084700+priyanshu1443@users.noreply.github.com>
2023-10-23 00:13:26 +0530

    commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Basic Live Code Editor
+
+An Online Editor that compiles your code live with HTML/CSS/JS
+
+## Features
+
+- It will be a code editor with live preview similar to [Codepen](https://codepen.io/pen/) having 3 input text areas and 1 output area
+- Write a function in app.js to show the compiled code back on the same page
+- Making it look decent with style.css
+- Make the textareas responsive with media queries
+
+## Tech Stack
+
+- HTML
+- CSS
+- JS
+- (No Libraries/Frameworks)
---
+++ app.js
@@ -0,0 +1,139 @@
+var html = document.getElementById('html');
+var css = document.getElementById('css');
+var js = document.getElementById('js');
+var code = document.getElementById('output').contentWindow.document;
+function compile() {
+  const PREFIX = 'livecode-';
+  const data = ['html', 'css', 'js'].map((key) => {
+    const prefixedKey = PREFIX + key;
+    const jsonValue = localStorage.getItem(prefixedKey);
+
+    if (jsonValue != null) return JSON.parse(jsonValue);
+  });
+  setInitial(data);
+  document.body.onkeyup = function () {
+    localStorage.setItem('livecode-html', JSON.stringify(html.value));
+    localStorage.setItem('livecode-css', JSON.stringify(css.value));
+    localStorage.setItem('livecode-js', JSON.stringify(js.value));
+    code.open();
+    code.writeln(
+      html.value +
+      '<style>' +
+      css.value +
+      '</style>' +
+      '<script>' +
+      js.value +
+      '</script>'
+    );
+    code.close();
+  };
+}
+
+function setInitial(data) {
+  let htmlContent = data[0] || '<h1>Welcome to the Code Editor!</h1>';
+  let cssContent =
+    data[1] ||
+    `body {
+    background-color: #222;
+    }
+    h1 {
+      color: #fff;
+      text-align: center;
+      margin-top: 10%;
+    }`;
+  let jsContent = data[2] || '';
+  css.value = cssContent;
+  js.value = jsContent;
+  html.value = htmlContent;
+  code.open();
+  code.writeln(
+    htmlContent +
+    '<style>' +
+    cssContent +
+    '</style>' +
+    '<script>' +
+    jsContent +
+    '</script>'
+  );
+  code.close();
+}
+
+compile();
+
+document.querySelectorAll('.control').forEach((control) =>
+  control.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.classList.toggle('collapse');
+    e.target.classList.add('close');
+    e.target.parentElement.querySelector('h2').classList.toggle('hidden');
+  })
+);
+
+document.querySelectorAll('.clear').forEach((clear) =>
+  clear.addEventListener('click', (e) => {
+    const ele = e.target.classList[1];
+    document.querySelector(`#${ele}`).value = '';
+    localStorage.setItem(`livecode-${ele}`, JSON.stringify(''));
+    compile();
+  })
+);
+
+document.querySelectorAll('.copy-btn').forEach((copy) => {
+  copy.addEventListener('click', (e) => {
+    const temp = e.target.innerHTML;
+    e.target.innerText = 'Copied!';
+    setTimeout(function () {
+      e.target.innerHTML = temp;
+    }, 800);
+  });
+});
+
+document.querySelector('.copy-html').addEventListener('click', (e) => {
+  const code = document.querySelector('#html');
+  copyCode(code);
+});
+
+document.querySelector('.copy-css').addEventListener('click', (e) => {
+  const code = document.querySelector('#css');
+  copyCode(code);
+});
+document.querySelector('.copy-js').addEventListener('click', (e) => {
+  const code = document.querySelector('#js');
+  copyCode(code);
+});
+
+function copyCode(code) {
+  code.select();
+  document.execCommand('copy');
+  swal('Copied!', 'You are ready to rock', 'success');
+}
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = document.getElementById("html");
+  const css = document.getElementById('css');
+  const js = document.getElementById('js')
+  const saveButton = document.getElementById("saveButton");
+
+  saveButton.addEventListener("click", function () {
+    const textContent = "<html>\n<Head> \n<style>\n" + css.value + "\n</style>\n</Head>\n <body>\n" + html.value + "\n<script>\n" + js.value + "</script>\n </body>\n </html>";
+    const blob = new Blob([textContent], { type: "text/plain" });
+    const filename = "text.html"; // Set the desired filename here
+
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = filename;
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+});
